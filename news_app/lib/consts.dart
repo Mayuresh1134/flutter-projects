6c69c87407fe7d00f6ef436d7ce6446c388a54be
Mayuresh1134 <mspednekar6603@gmail.com>
2024-06-03 14:08:43 +0530

    first commit

---
+++ news_app/lib/consts.dart
@@ -0,0 +1,4 @@
+const String NEWS_API_KEY = "56cfd553e0ec4ba5b4a2031f4afd061f";
+
+const String PLACEHOLDER_IMAGE_LINK =
+    "https://developers.elementor.com/docs/assets/img/elementor-placeholder-image.png";
